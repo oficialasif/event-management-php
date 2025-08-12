407625b150aa8c5cbcab1dc9d44d4df6e6b10f37
Asif Mahmud <asifmahmud802@gmail.com>
2025-08-12 09:59:08 +0600

    Add favicon to all pages

--- register.php
+++ register.php
@@ -68,6 +68,7 @@ if ($_POST && isset($_POST['register'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register - EventHub</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
